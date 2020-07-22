d3d25b7cf8703f84bc968b33bfbe1d8ff968313e
adewaleK <bisdam91@gmail.com>
2020-07-22 08:56:11 +0100

    adds user_param method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,19 @@ class UsersController < ApplicationController
     @user = User.new
   end
     
-def create
-  @user = User.new(user_params)
+  def create
+    @user = User.new(user_params)
     if @user.save
       redirect_to new_user_path
     else
       render :new
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
 end
